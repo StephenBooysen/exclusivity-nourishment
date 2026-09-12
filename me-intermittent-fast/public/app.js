34dfc718e70ffcfea7e4551e454cf4dcc5d71179
Stephen Booysen <stephenrbooysen@gmail.com>
2026-09-12 11:32:07 +0000

    chore: remove embedded nested directory (me-intermittent-fast)

--- me-intermittent-fast/public/app.js
+++
@@ -1,9 +0,0 @@
-import { initRouter } from './js/router.js';
-
-initRouter();
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js').catch((err) => console.warn('Service worker registration failed', err));
-  });
-}
